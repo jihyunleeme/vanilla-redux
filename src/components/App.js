1895e055d25eabb6db63ed7f6e6580bdae38de48
dream_JHL <jihyunlee.me@gmail.com>
2021-08-06 18:02:23 +0900

    #3.1 Connecting the store

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import { useSelector, useDispatch } from 'react-redux';
+import { BrowserRouter as Router, Switch, useLocation } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App"></div>
+  );
+}
+
+export default App;
